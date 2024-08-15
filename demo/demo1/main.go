bc57705ed9360400d6fbe9c67ec25dc72735073f
Your Name <you@example.com>
2024-08-16 02:55:17 +0800

    .

---
+++ demo/demo1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	cherry "github.com/beijian01/xgame/framework"
+	"github.com/beijian01/xgame/framework/net/profile"
+	"sync"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		gate1 := cherry.NewAppNode(&profile.NodeCfg{
+			NodeId:   "gate1",
+			NodeType: "gate",
+			Address:  "",
+		}, true)
+		gate1.Startup()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		game1 := cherry.NewAppNode(&profile.NodeCfg{
+			NodeId:   "game1",
+			NodeType: "game",
+			Address:  "",
+		}, false)
+		game1.Startup()
+	}()
+
+}
